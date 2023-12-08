afd36298246ee09f7f1e7cdc07a13fa1d18b77fc
Ranadeep Biswas <mail@rnbguy.at>
2023-12-08 02:37:48 +0100

    add native tailwindcss

--- main.ts
+++ main.ts
@@ -7,14 +7,14 @@
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 
-import twindPlugin from "$fresh/plugins/twind.ts";
-import twindConfig from "./twind.config.ts";
+import tailwindPlugin from "$fresh/plugins/tailwind.ts";
+import tailwindConfig from "./tailwind.config.ts";
 
 const config = Deno.env.toObject();
 
 const FRESH_PORT_ID = parseInt(config["FRESH_SERVER_PORT"] || "8000");
 
 await start(manifest, {
-  plugins: [twindPlugin(twindConfig)],
+  plugins: [tailwindPlugin(tailwindConfig)],
   port: FRESH_PORT_ID,
 });
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component }: AppProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>fresh-playground</title>
+        <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
         <Component />
