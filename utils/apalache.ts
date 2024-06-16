11723ce3877abf5c0384788a8e0f11bf073a624a
Ranadeep Biswas <mail@rnbguy.at>
2024-06-16 20:41:37 +0200

    use native grpc client

--- utils/apalache.ts
+++ utils/apalache.ts
@@ -1,24 +1,61 @@
 import { Untar } from "@std/archive";
 import { readerFromStreamReader } from "@std/io";
 import { copy } from "@std/io";
-import { getClient, GrpcClient } from "grpc_basic/client.ts";
 import { cache } from "cache/mod.ts";
+import { promisify } from "node:util";
+import { default as protobufDescriptor } from "@protobufjs/ext/descriptor/index.js";
+import * as grpc from "@grpc/grpc-js";
+import * as proto from "@grpc/proto-loader";
+import { getReflectionClient, ServerReflectionResponse } from "./reflection.ts";
 
-import { default as protobuf } from "protobufjs";
-import { default as protobufDescriptor } from "protobufjs/ext/descriptor";
-
-const REFLECTION_PROTO_URL =
-  "https://github.com/grpc/grpc/raw/master/src/proto/grpc/reflection/v1alpha/reflection.proto";
-const GRPC_REFLECTION_SERVICE_NAME = "grpc.reflection.v1alpha.ServerReflection";
+// const REFLECTION_PROTO_URL =
+//   "https://github.com/grpc/grpc/raw/master/src/proto/grpc/reflection/v1alpha/reflection.proto";
 
 const GH_REPO = "informalsystems/apalache";
 const TGZ_JAR_NAME = "apalache.jar";
 const APALACHE_SERVICE_NAME = "shai.cmdExecutor.CmdExecutor";
 
+type RunRequest = { cmd: string; config: string };
+type PingRequest = Record<number, never>;
+
+type RunResponse =
+  | { result: "failure"; failure: { errorType: string; data: string } }
+  | { result: "success" };
+
+type PingResponse = Record<number, never>;
+
+type CmdExecutor = {
+  new (url: string, creds: grpc.ChannelCredentials): CmdExecutor;
+  run: (
+    req: RunRequest,
+    callback: (err: any, res: RunResponse) => void,
+  ) => void;
+  ping: (
+    req: PingRequest,
+    callback: (err: any, res: PingResponse) => void,
+  ) => void;
+};
+
+type ShaiPkg = {
+  shai: {
+    cmdExecutor: {
+      CmdExecutor: CmdExecutor;
+    };
+  };
+};
+
+const grpcStubOptions = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+};
+
 export class Apalache {
   version: string | undefined;
   process: Deno.ChildProcess | undefined;
-  client: GrpcClient | undefined;
+  client: CmdExecutor | undefined;
 
   async setVersion(version: string) {
     if (version == "latest") {
@@ -33,39 +70,21 @@ export class Apalache {
     return resp.tag_name;
   }
 
-  async getCmdExecutorProto(
+  getCmdExecutorService(
+    packageDefinition: proto.PackageDefinition,
     connectOption: Deno.ConnectOptions,
-  ): Promise<protobuf.Root> {
-    const reflectionProto: string = await fetch(REFLECTION_PROTO_URL).then((
-      resp,
-    ) => resp.text());
+  ): CmdExecutor {
+    // TODO(rano): would be nice to return protobuf.Root here.
+    // how can we use protobuf.Root.fromDescriptor ?
 
-    const reflectionClient = getClient({
-      root: reflectionProto,
-      serviceName: GRPC_REFLECTION_SERVICE_NAME,
-      ...connectOption,
-    });
+    const apalacheService = grpc.loadPackageDefinition(
+      packageDefinition,
+    ) as unknown as ShaiPkg;
 
-    const respStream = reflectionClient.ServerReflectionInfo({
-      fileContainingSymbol: APALACHE_SERVICE_NAME,
-    });
-
-    const resp = await respStream.next();
-    respStream.return();
-    reflectionClient.close();
-
-    const fileDescriptorProtos = resp.value.fileDescriptorResponse
-      .fileDescriptorProto.map((buf) =>
-        protobufDescriptor.FileDescriptorProto.decode(
-          buf,
-        )
-      );
-
-    const fileDescriptorSet = protobufDescriptor.FileDescriptorSet.fromObject({
-      file: fileDescriptorProtos,
-    });
-
-    return protobuf.Root.fromDescriptor(fileDescriptorSet);
+    return new apalacheService.shai.cmdExecutor.CmdExecutor(
+      `${connectOption.hostname}:${connectOption.port}`,
+      grpc.credentials.createInsecure(),
+    );
   }
 
   getJarName(): string {
@@ -123,12 +142,47 @@ export class Apalache {
       );
       port = APALACHE_PORT_ID;
     }
-    this.client = getClient({
-      hostname,
-      port,
-      root: await this.getCmdExecutorProto({ hostname, port }),
-      serviceName: APALACHE_SERVICE_NAME,
+
+    const conn_opt = { hostname: hostname || "localhost", port: port };
+
+    const reflectionClient = await getReflectionClient(conn_opt);
+
+    // Query reflection endpoint (retry if server is unreachable)
+    const apalacheProtoDef: proto.PackageDefinition = await new Promise<
+      ServerReflectionResponse
+    >(
+      (resolve, reject) => {
+        const call = reflectionClient.ServerReflectionInfo();
+        call.on("data", (r: ServerReflectionResponse) => {
+          call.end();
+          resolve(r);
+        });
+        call.on("error", (e: grpc.StatusObject) => reject(e));
+
+        call.write({ file_containing_symbol: APALACHE_SERVICE_NAME });
+      },
+    ).then((protoDefResponse: ServerReflectionResponse) => {
+      if ("error_response" in protoDefResponse) {
+        return Promise.reject(protoDefResponse);
+      } else {
+        // Decode reflection response to FileDescriptorProto
+        const fileDescriptorProtos = protoDefResponse.file_descriptor_response
+          .file_descriptor_proto.map(
+            (bytes) =>
+              protobufDescriptor.FileDescriptorProto.decode(
+                bytes,
+              ) as protobufDescriptor.IFileDescriptorProto,
+          );
+
+        // Use proto-loader to load the FileDescriptorProto wrapped in a FileDescriptorSet
+        return proto.loadFileDescriptorSetFromObject(
+          { file: fileDescriptorProtos },
+          grpcStubOptions,
+        );
+      }
     });
+
+    this.client = this.getCmdExecutorService(apalacheProtoDef, conn_opt);
   }
 
   async modelCheck(tla: string, inv: string, length: number): Promise<any> {
@@ -152,10 +206,15 @@ export class Apalache {
     };
 
     const cmd = {
-      cmd: this.client.svc.lookup("Cmd").values.CHECK,
+      // cmd: this.client.svc.lookup("Cmd").values.CHECK,
+      cmd: "CHECK",
       config: JSON.stringify(config),
     };
-    const resp = await this.client.run(cmd);
+
+    const resp = await promisify((
+      data: RunRequest,
+      callback: (err: any, res: RunResponse) => void,
+    ) => this.client?.run(data, callback))(cmd);
     return resp;
   }
 
@@ -176,10 +235,22 @@ export class Apalache {
     };
 
     const cmd = {
-      cmd: this.client.svc.lookup("Cmd").values.SIMULATE,
+      // cmd: this.client.svc.lookup("Cmd").values.SIMULATE,
+      cmd: "SIMULATE",
       config: JSON.stringify(config),
     };
-    const resp = await this.client.run(cmd);
+    const resp = await promisify((
+      data: RunRequest,
+      callback: (err: any, res: RunResponse) => void,
+    ) => this.client?.run(data, callback))(cmd);
+    return resp;
+  }
+
+  async ping(): Promise<any> {
+    const resp = await promisify((
+      data: PingRequest,
+      callback: (err: any, res: PingResponse) => void,
+    ) => this.client?.ping(data, callback))({});
     return resp;
   }
 }
---
+++ utils/reflection.ts
@@ -0,0 +1,65 @@
+import { default as protobuf } from "@protobufjs";
+
+import { Buffer } from "node:buffer";
+
+import * as grpc from "@grpc/grpc-js";
+import * as proto from "@grpc/proto-loader";
+
+const PROTO_URL =
+  "https://github.com/grpc/grpc/raw/master/src/proto/grpc/reflection/v1alpha/reflection.proto";
+// const GRPC_REFLECTION_SERVICE_NAME = "grpc.reflection.v1alpha.ServerReflection";
+
+// Types of the gRPC interface.
+type ServerReflectionRequest = { file_containing_symbol: string };
+type ServerReflectionResponseSuccess = {
+  file_descriptor_response: {
+    file_descriptor_proto: Buffer[];
+  };
+};
+type ServerReflectionResponseFailure = {
+  error_response: {
+    error_code: number;
+    error_message: string;
+  };
+};
+export type ServerReflectionResponse =
+  | ServerReflectionResponseSuccess
+  | ServerReflectionResponseFailure;
+type ServerReflectionService = {
+  new (url: string, creds: grpc.ChannelCredentials): ServerReflectionService;
+  ServerReflectionInfo: () => grpc.ClientDuplexStream<
+    ServerReflectionRequest,
+    ServerReflectionResponse
+  >;
+};
+type ServerReflectionPkg = {
+  grpc: {
+    reflection: { v1alpha: { ServerReflection: ServerReflectionService } };
+  };
+};
+
+const grpcStubOptions = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+};
+
+export async function getReflectionClient(
+  connectionOption: Deno.ConnectOptions,
+): Promise<ServerReflectionService> {
+  const response = await fetch(PROTO_URL);
+  const protoJson = protobuf.parse(await response.text(), grpcStubOptions).root
+    .toJSON();
+  const packageDefinition = proto.fromJSON(protoJson, grpcStubOptions);
+  // const packageDefinition = proto.loadSync(protoLocalPath, grpcStubOptions);
+  const reflectionProtoDescriptor = grpc.loadPackageDefinition(
+    packageDefinition,
+  ) as unknown as ServerReflectionPkg;
+  return new reflectionProtoDescriptor.grpc.reflection.v1alpha
+    .ServerReflection(
+    `${connectionOption.hostname}:${connectionOption.port}`,
+    grpc.credentials.createInsecure(),
+  );
+}
