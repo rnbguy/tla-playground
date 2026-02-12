81d33e97e9563836b7a0df7b732bb3f3c650d08d
Ranadeep Biswas <mail@rnbguy.at>
2026-02-12 16:04:48 +0100

    bring back tiny boot shim

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component }: PageProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>TLA+ Playground</title>
+        <script>var r = null;</script>
       </head>
       <body>
         <Component />
